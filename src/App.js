f50e3647550b58f3eee5ab4a6cb4c7508ebbb95b
Zixi Tang <zixi.sam.tang@gmail.com>
2026-04-13 13:58:08 -0400

    Forgot to add a file to last commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import './App.css';
-import { About } from './components/About';
-import { Navbar } from './components/Navbar';
-import { Projects } from './components/Projects';
-import { Contact } from './components/Contact';
+import "./App.css";
+import { About } from "./components/About";
+import { Navbar } from "./components/Navbar";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <About/>
-      <Projects/>
-      <Contact/>
+    <div className="App app-shell">
+      <div className="page-glow page-glow-one" aria-hidden="true" />
+      <div className="page-glow page-glow-two" aria-hidden="true" />
+      <Navbar />
+      <main className="app-main">
+        <About />
+        <Projects />
+      </main>
+      <Contact />
     </div>
   );
 }
